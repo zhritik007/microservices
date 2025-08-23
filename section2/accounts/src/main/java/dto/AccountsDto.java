897de3200abe3799c3c3fbff5660f2310e51c983
Hritik Singh <ghritiksingh21@gmail.com>
2025-08-23 21:07:34 +0530

    Initial commit

---
+++ section2/accounts/src/main/java/dto/AccountsDto.java
@@ -0,0 +1,14 @@
+package dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
---
+++ section2/accounts/src/main/java/entity/Accounts.java
@@ -0,0 +1,32 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity {
+
+    @Column(name= "customer_id")
+    private long customerId;
+
+    @Id
+    @Column(name="account_number")
+    private Long accountNumber;
+
+    @Column(name="account_type")
+    private String accountType;
+
+    @Column(name="branch_address")
+    private String branchAddress;
+}
